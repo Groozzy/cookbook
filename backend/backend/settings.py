ca66f686203989342cd24f6dbd72b7bbb100abc0
Соловьёв Андрей Андреевич <groozzy@gmail.com>
2023-12-03 09:05:07 +0300

    Fix workflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'https://foodgram.servepics.com/media/'
 MEDIA_ROOT = '/var/www/foodgram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
