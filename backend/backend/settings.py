569625c28d5416cae478f779a20a797ef7b6094b
Соловьёв Андрей Андреевич <groozzy@gmail.com>
2023-12-05 01:49:32 +0300

    minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 ROOT_URLCONF = 'backend.urls'
 
-CSRF_TRUSTED_ORIGINS = (os.getenv('PROJECT_URL'),)
+CSRF_TRUSTED_ORIGINS = (os.getenv('PROJECT_URL', ''),)
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
